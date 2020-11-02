b6717af384112e786f8f8624d7e6a35fe694664d
mmoranodevelop <61603158+mmoranodevelop@users.noreply.github.com>
2020-11-02 14:09:21 +0100

    final fix limit 40

--- src/api/flyers-api.js
+++ src/api/flyers-api.js
@@ -3,7 +3,7 @@ import Axios from 'axios'
 
 export async function getFlyers(page: number) {
     console.log('Fetching flyers')
-    const response = await Axios.get(`${apiUrl}/flyers?page=${JSON.stringify(page)}&limit=50`, {
+    const response = await Axios.get(`${apiUrl}/flyers?page=${JSON.stringify(page)}&limit=40`, {
         headers: {
             'Content-Type': 'application/json'
         },
